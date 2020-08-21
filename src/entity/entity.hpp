dd6b8f2e4a81400150c89edc7fb4490773e1fe7c
urlordjames <urlordjames@gmail.com>
2020-08-21 18:39:00 -0400

    changed stuff with cmake

---
+++ src/entity/entity.hpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include "SDL.h"
+
+class DrawComponent {
+protected:
+	SDL_Texture *tex;
+public:
+	double rot;
+	SDL_Rect rect;
+	DrawComponent(double rotation, SDL_Rect rectangle, SDL_Texture* texture) {
+		rot = rotation;
+		rect = rectangle;
+		tex = texture;
+	}
+
+	SDL_Texture* getTexture() {
+		return tex;
+	}
+};
+
+class Entity {
+public:
+	std::vector<DrawComponent *> textures;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
-#include <vector>
-#include <memory>
 #include <cmath>
 #include <algorithm>
 #include "SDL.h"
+#include "entity.hpp"
 
 SDL_Texture* loadtexture(SDL_Renderer* r, const char *str) {
     SDL_Surface* s = SDL_LoadBMP(str);
@@ -12,40 +11,13 @@ SDL_Texture* loadtexture(SDL_Renderer* r, const char *str) {
     return t;
 }
 
-class DrawComponent {
-protected:
-	SDL_Texture *tex;
-public:
-	double rot;
-	SDL_Rect rect;
-	DrawComponent(double rotation, SDL_Rect rectangle, SDL_Texture* texture) {
-		rot = rotation;
-		rect = rectangle;
-		tex = texture;
-	}
-
-	SDL_Texture* getTexture() {
-		return tex;
-	}
-};
-
-class Entity {
-public:
-	std::vector<DrawComponent *> textures;
-};
-
 void drawEntity(SDL_Renderer* r, Entity* entity) {
 	for (const auto drawcomp : entity->textures) {
 		SDL_RenderCopyEx(r, drawcomp->getTexture(), NULL, &drawcomp->rect, drawcomp->rot, NULL, SDL_FLIP_NONE);
 	}
 }
 
-int main(int, char **) {
-    SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window *w = SDL_CreateWindow("dog", 100, 100, 500, 500, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
-    SDL_Renderer *r = SDL_CreateRenderer(w, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    SDL_SetRenderDrawColor(r, 0, 0, 255, 255);
-
+Entity* getDog(SDL_Renderer* r) {
     SDL_Texture *t = loadtexture(r, "assets/dog.bmp");
     SDL_Rect rect;
     rect.x = 0;
@@ -56,7 +28,16 @@ int main(int, char **) {
 
     Entity *dogent = new Entity;
     dogent->textures.push_back(dogcomp);
+    return dogent;
+}
 
+int main(int, char **) {
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window *w = SDL_CreateWindow("dog", 100, 100, 500, 500, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+    SDL_Renderer *r = SDL_CreateRenderer(w, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    SDL_SetRenderDrawColor(r, 0, 0, 255, 255);
+    Entity *dogent = getDog(r);
+    DrawComponent *dogcomp = dogent->textures.at(0);
     int deltatime = 0;
     while (true) {
 	int starttime = SDL_GetTicks();

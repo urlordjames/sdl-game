94e78cda5b0f33d219896ee377e88c1fa2e0d875
urlordjames <urlordjames@gmail.com>
2020-08-23 19:54:55 -0400

    linted source files

--- src/entity/entity.hpp
+++ src/entity/entity.hpp
@@ -1,24 +1,24 @@
+// Copyright 2020 urlordjames
 #include <vector>
 #include "SDL.h"
 
 class DrawComponent {
-protected:
-	SDL_Texture *tex;
-public:
-	double rot;
-	SDL_Rect rect;
-	DrawComponent(double rotation, SDL_Rect rectangle, SDL_Texture* texture) {
-		rot = rotation;
-		rect = rectangle;
-		tex = texture;
-	}
-
-	SDL_Texture* getTexture() {
-		return tex;
-	}
+ protected:
+  SDL_Texture *tex;
+ public:
+  double rot;
+  SDL_Rect rect;
+  DrawComponent(double rotation, SDL_Rect rectangle, SDL_Texture* texture) {
+    rot = rotation;
+    rect = rectangle;
+    tex = texture;
+  }
+  SDL_Texture* getTexture() {
+    return tex;
+  }
 };
 
 class Entity {
-public:
-	std::vector<DrawComponent *> textures;
+ public:
+  std::vector<DrawComponent *> textures;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+// Copyright 2020 urlordjames
 #include <iostream>
 #include <cmath>
 #include <algorithm>
@@ -5,58 +6,74 @@
 #include "entity.hpp"
 
 SDL_Texture* loadtexture(SDL_Renderer* r, const char *str) {
-    SDL_Surface* s = SDL_LoadBMP(str);
-    SDL_Texture* t = SDL_CreateTextureFromSurface(r, s);
-    SDL_FreeSurface(s);
-    return t;
+  SDL_Surface* s = SDL_LoadBMP(str);
+  SDL_Texture* t = SDL_CreateTextureFromSurface(r, s);
+  SDL_FreeSurface(s);
+  return t;
 }
 
 void drawEntity(SDL_Renderer* r, Entity* entity) {
-	for (const auto drawcomp : entity->textures) {
-		SDL_RenderCopyEx(r, drawcomp->getTexture(), NULL, &drawcomp->rect, drawcomp->rot, NULL, SDL_FLIP_NONE);
-	}
+  for (const auto drawcomp : entity->textures) {
+    SDL_RenderCopyEx(r,
+                    drawcomp->getTexture(),
+                    NULL,
+                    &drawcomp->rect,
+                    drawcomp->rot,
+                    NULL,
+                    SDL_FLIP_NONE);
+  }
 }
 
 Entity* getDog(SDL_Renderer* r) {
-    SDL_Texture *t = loadtexture(r, "assets/dog.bmp");
-    SDL_Rect rect;
-    rect.x = 0;
-    rect.y = 0;
-    rect.w = 1;
-    rect.h = 1;
-    DrawComponent *dogcomp = new DrawComponent(0, rect, t);
-
-    Entity *dogent = new Entity;
-    dogent->textures.push_back(dogcomp);
-    return dogent;
+  SDL_Texture *t = loadtexture(r, "assets/dog.bmp");
+  SDL_Rect rect;
+  rect.x = 0;
+  rect.y = 0;
+  rect.w = 64;
+  rect.h = 64;
+  DrawComponent *dogcomp = new DrawComponent(0, rect, t);
+  Entity *dogent = new Entity;
+  dogent->textures.push_back(dogcomp);
+  return dogent;
 }
 
 int main(int, char **) {
-    SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window *w = SDL_CreateWindow("dog", 100, 100, 500, 500, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
-    SDL_Renderer *r = SDL_CreateRenderer(w, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    SDL_SetRenderDrawColor(r, 0, 0, 255, 255);
-    Entity *dogent = getDog(r);
-    DrawComponent *dogcomp = dogent->textures.at(0);
-    int deltatime = 0;
-    while (true) {
-	int starttime = SDL_GetTicks();
-	SDL_RenderClear(r);
-	drawEntity(r, dogent);
-	dogcomp->rect.w = dogcomp->rect.w + 1;
-	dogcomp->rect.h = dogcomp->rect.h + 1;
-	dogcomp->rot = std::fmod(dogcomp->rot + 1, 360);
-        SDL_RenderPresent(r);
-        SDL_Event event;
-        SDL_PollEvent(&event);
-        if (event.type == SDL_QUIT) {
-            break;
+  SDL_Init(SDL_INIT_EVERYTHING);
+  SDL_Window *w = SDL_CreateWindow("dog",
+                                  100,
+                                  100,
+                                  500,
+                                  500,
+                                  SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+  SDL_Renderer *r = SDL_CreateRenderer(w,
+                                      -1,
+                                      SDL_RENDERER_ACCELERATED |
+                                      SDL_RENDERER_PRESENTVSYNC);
+  SDL_SetRenderDrawColor(r, 0, 0, 255, 255);
+  Entity *dogent = getDog(r);
+  DrawComponent *dogcomp = dogent->textures.at(0);
+  int deltatime = 0;
+  bool quit = false;
+  while (!quit) {
+  int starttime = SDL_GetTicks();
+  SDL_RenderClear(r);
+  drawEntity(r, dogent);
+  SDL_RenderPresent(r);
+  SDL_Event event;
+  while (SDL_PollEvent(&event)) {
+      switch (event.type) {
+        case SDL_QUIT:
+          quit = true;
+          break;
+        case SDL_KEYDOWN:
+          break;
         }
-	SDL_Delay(std::max(30 - deltatime, 0));
-	deltatime = SDL_GetTicks() - starttime;
     }
-    SDL_DestroyRenderer(r);
-    SDL_DestroyWindow(w);
-    SDL_Quit();
-    return 0;
+    SDL_Delay(std::max(30 - deltatime, 0));
+    deltatime = SDL_GetTicks() - starttime;
+  }
+  SDL_DestroyRenderer(r);
+  SDL_DestroyWindow(w);
+  SDL_Quit();
+  return 0;
 }
